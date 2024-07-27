8bd66413475e20b37c7afb7d93e698335d88a6ab
PapaDaCodr <dukeoriginal6@gmail.com>
2024-07-27 03:51:38 +0000

    user progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,13 +1,19 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage =() => {
     return(
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <div>
                 <StickyWrapper>
-                    My sticky wrapper
+                    <UserProgress 
+                    activeCourse={{title: "ICT", imageSrc:"/ICT.svg" }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                    />
                 </StickyWrapper>
             </div>
             <div>
