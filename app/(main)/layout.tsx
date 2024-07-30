c0ed367d43c2fdd5e7e9b5bc6d24bf563febdfaf
PapaDaCodr <dukeoriginal6@gmail.com>
2024-07-30 09:01:52 +0000

    user progress schema added to the database

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -10,12 +10,12 @@ const MainLayout = ({children,}: Props) => {
        <>
             <MobileHeader />
             <Sidebar className="hidden lg:flex" />
-            <main className="pl-[256px] h-full pt-[50px] lg:pt-0">
-                 <div className="max-w-[1056px] mx-auto pt-6 h-full">
+            <main className="pl-[256px] h-full pt-[50px] lg:pt-0 ">
+                 <div className="max-w-[1056px] mx-auto pt-6 h-full ">
                  {children}
                  </div>
             </main>
-            </>
+        </>
     );
 };
 
--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
    
    return(
    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200-p-2">
-        
+        Quizard &copy; Copyright 2024    
     </footer>
    )
 }
