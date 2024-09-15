32bc1536ce8d4b538af15476b6df75995354511f
PapaDaCodr <dukeoriginal6@gmail.com>
2024-09-15 15:47:27 +0000

    no change

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -26,14 +26,13 @@ const LessonPage = async () => {
     .length / lesson.challenges.length * 100;
 
   return (
-     <Quiz
-       initialLessonId={lesson.id}
-       initialLessonChallenges={lesson.challenges}
-       initialHearts={userProgress.hearts}
-       initialPercentage={initialPercentage}
-       userSubscription={userSubscription}
+    <Quiz
+      initialLessonId={lesson.id}
+      initialLessonChallenges={lesson.challenges}
+      initialHearts={userProgress.hearts}
+      initialPercentage={initialPercentage}
+      userSubscription={userSubscription}
     />
-  
   )
 }
 
