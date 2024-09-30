2ef269c320b2685d7feebdb07682599b1c88865b
PapaDaCodr <131633759+PapaDaCodr@users.noreply.github.com>
2024-09-30 11:59:10 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" />
 
       <body className={font.className}>
       <Toaster />
